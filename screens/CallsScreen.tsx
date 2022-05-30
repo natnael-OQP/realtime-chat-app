72c3ab4da55e51d9f92f158c221ecf78df6c100b
natnael-OQP <makiya112545@gmail.com>
2022-05-30 20:57:09 +0300

    create callsScreen

---
+++ screens/CallsScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import { Text } from '../components/Themed'
+
+const CallsScreen = () => {
+    return (
+        <View>
+            <Text>CallsScreen</Text>
+        </View>
+    )
+}
+
+export default CallsScreen
+
+const styles = StyleSheet.create({})
