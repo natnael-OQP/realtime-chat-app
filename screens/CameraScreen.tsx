ba67f251c6ab753c400a33766a3a2252647c4de8
natnael-OQP <makiya112545@gmail.com>
2022-05-31 22:45:53 +0300

    created camera screen

---
+++ screens/CameraScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import { Text } from '../components/Themed'
+
+const CameraScreen = () => {
+    return (
+        <View>
+            <Text>CameraScreen</Text>
+        </View>
+    )
+}
+
+export default CameraScreen
+
+const styles = StyleSheet.create({})
