68aa95bd0f3b7c6d2a2ef6d73d03a8a14c24e896
natnael-OQP <makiya112545@gmail.com>
2022-05-31 22:46:10 +0300

    render ChatListItem

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
+import ChatRooms from '../assets/data/ChatRooms'
+import ChatListItem from '../components/ChatListItem'
 
 import { Text, View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
@@ -6,7 +8,12 @@ import { RootTabScreenProps } from '../types'
 export default function ChatScreen({ navigation }: RootTabScreenProps<'Chat'>) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Chat</Text>
+            <FlatList
+                style={{ width: '100%' }}
+                data={ChatRooms}
+                renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     )
 }
@@ -15,7 +22,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
     },
     title: {
         fontSize: 20,
