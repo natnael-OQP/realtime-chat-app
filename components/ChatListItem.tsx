f945a8e76798de37ea20b2a440922bebf3682857
natnael-OQP <makiya112545@gmail.com>
2022-06-01 23:27:38 +0300

    componetaize the avatar

--- components/ChatListItem.tsx
+++ components/ChatListItem.tsx
@@ -1,23 +1,45 @@
-import { Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Text, View } from './Themed'
 import { ChatRoomProps, UserProps } from '../types'
+import useColorScheme from '../hooks/useColorScheme'
+import Avatar from './Avatar'
 
 interface Props {
     chatRoom: ChatRoomProps
 }
 
 const ChatListItem = ({ chatRoom: { users, lastMessage } }: Props) => {
-    const { imageUri, name } = users[1]
     const { content, createdAt } = lastMessage
+    const theme = useColorScheme()
 
     return (
-        <TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.65}>
             <View style={styles.container}>
-                <Image style={styles.image} source={{ uri: imageUri }} />
+                <Avatar users={users} width={60} />
                 <View style={styles.userNameContainer}>
-                    <Text>{name}</Text>
-                    <Text>{content}</Text>
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <Text style={styles.name}>{users[1].name}</Text>
+                        <Text
+                            style={{
+                                marginLeft: 10,
+                                fontSize: 12,
+                                color: 'gray',
+                            }}
+                        >
+                            {createdAt}
+                        </Text>
+                    </View>
+                    <Text style={{ color: 'gray' }}>{content}</Text>
+                    <View
+                        style={[
+                            styles.separator,
+                            {
+                                backgroundColor:
+                                    theme == 'dark' ? '#262626' : '#F2F2F2',
+                            },
+                        ]}
+                    />
                 </View>
             </View>
         </TouchableOpacity>
@@ -32,13 +54,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 10,
     },
-    image: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-    },
     userNameContainer: {
         marginLeft: 10,
+        flex: 1,
     },
-    name: {},
+    name: {
+        marginBottom: 5,
+    },
+    separator: {
+        width: '95%',
+        height: 0.6,
+        marginTop: 15,
+    },
 })
