e1296448a9ab23ae16bcf9d280b9285d5c6d7ff2
natnael-OQP <makiya112545@gmail.com>
2022-06-02 14:00:15 +0300

    create ChatRoom

---
+++ screens/ChatRoomScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { Text, View } from '../components/Themed'
+
+const ChatRoomScreen = () => {
+    return (
+        <View>
+            <Text>ChatRoomScreen</Text>
+        </View>
+    )
+}
+
+export default ChatRoomScreen
+
+const styles = StyleSheet.create({})
