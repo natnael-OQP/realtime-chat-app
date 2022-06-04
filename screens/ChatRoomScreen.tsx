58bc998bf7ce840c8301f6a739fc2dd26b3caaed
natnael-OQP <makiya112545@gmail.com>
2022-06-04 23:31:40 +0300

    get id

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -2,7 +2,11 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import { Text, View } from '../components/Themed'
 
-const ChatRoomScreen = () => {
+const ChatRoomScreen = ({ route }: any) => {
+    const {
+        params: { id },
+    } = route
+
     return (
         <View>
             <Text>ChatRoomScreen</Text>
